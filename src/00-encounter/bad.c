cf4d4e5c0fa471ca30a68b58d8524ba33ac73fd5
Tom Wilson <tom.andrew.wilson@gmail.com>
2022-01-23 17:39:52 -0500

    fix compile errors in bad.c

--- src/00-encounter/bad.c
+++ src/00-encounter/bad.c
@@ -1,23 +1,25 @@
 /* This may look like nonsense, but really is -*- mode: C -*- */
+#include <stdlib.h>
+#include <stdio.h>
 
 /* The main thing that this program does. */
-void main() {
+void main(void)
+{
   // Declarations
   int i;
   double A[5] = {
-    9.0,
-    2.9,
-    3.E+25,
-    .00007,
+      9.0,
+      2.9,
+      3.E+25,
+      .00007,
   };
 
   // Doing some work
-  for (i = 0; i < 5; ++i) {
-     printf("element %d is %g, \tits square is %g\n", /*@\label{printf-start-badly}*/
-            i,
-            A[i],
-            A[i]*A[i]);                               /*@\label{printf-end-badly}*/
+  for (i = 0; i < 5; ++i)
+  {
+    printf("element %d is %g, \tits square is %g\n", /*@\label{printf-start-badly}*/
+           i,
+           A[i],
+           A[i] * A[i]); /*@\label{printf-end-badly}*/
   }
-
-  return 0;                                           /*@\label{main-return-badly}*/
 }
